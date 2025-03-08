e52b658415c34e7e0a9589c556dc29721a24ca60
braddy78 <pvldvk@gmail.com>
2025-03-08 14:05:52 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -210,6 +210,12 @@
                 var choiceAnchorEl = choiceParagraphElement.querySelectorAll("a")[0];
                 choiceAnchorEl.addEventListener("click", function(event) {
 
+        // Google Analytics tracking for choices
+        gtag('event', 'choice_selected', {
+            'event_category': 'Story Choice',
+            'event_label': choice.text
+        });
+
                     // Don't follow <a> link
                     event.preventDefault();
 
